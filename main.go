440e454a600dded0d40650db42b2a525b15f7a79
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2013-11-30 17:40:37 +0100

    Adds gmc skeleton based on the go command line tool

---
+++ main.go
@@ -0,0 +1,159 @@
+// Copyright 2013 The gmc Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"text/template"
+)
+
+// A Command is an implementation of a gmc command
+type Command struct {
+	// Run runs the command.
+	// The args are the arguments after the command name.
+	Run func(args []string) int
+
+	// UsageLine is the one-line usage message.
+	// The first word in the line is taken to be the command name.
+	UsageLine string
+
+	// Short is the short description shown in the 'gmc help' output.
+	Short string
+
+	// Long is the long message shown in the 'gmc help <this-command>' output.
+	Long string
+
+	// Flag is a set of flags specific to this command.
+	Flag flag.FlagSet
+
+	// CustomFlags indicates that the command will do its own
+	// flag parsing.
+	CustomFlags bool
+}
+
+// Name returns the command's name: the first word in the usage line.
+func (c *Command) Name() string {
+	name := c.UsageLine
+	i := strings.Index(name, " ")
+	if i >= 0 {
+		name = name[:i]
+	}
+	return name
+}
+
+// Usage prints the command usage message and exits with code 2
+func (c *Command) Usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s\n\n", c.UsageLine)
+	fmt.Fprintf(os.Stderr, "%s\n", c.Long)
+	os.Exit(2)
+}
+
+// Runnable reports whether the command can be run; otherwise
+// it is a documentation pseudo-command such as importpath.
+func (c *Command) Runnable() bool {
+	return c.Run != nil
+}
+
+// Commands lists the available commands and help topics.
+// The order here is the order in which they are printed by 'gmc help'.
+var commands = []*Command{
+//cmdDummy,
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+	}
+
+	if args[0] == "help" {
+		help(args[1:])
+		return
+	}
+
+	for _, cmd := range commands {
+		if cmd.Name() == args[0] && cmd.Run != nil {
+			cmd.Flag.Usage = func() { cmd.Usage() }
+			if cmd.CustomFlags {
+				args = args[1:]
+			} else {
+				cmd.Flag.Parse(args[1:])
+				args = cmd.Flag.Args()
+			}
+			exitStatus := cmd.Run(args)
+			os.Exit(exitStatus)
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "gmc: unknown subcommand %q\nRun 'gmc help' for usage.\n", args[0])
+	os.Exit(2)
+}
+
+var usageTemplate = `gmc is a full featured commandline Unix like mail client
+
+Usage:
+    gmc command [arguments]
+
+The commands are:
+{{range .}}{{if .Runnable}}
+    {{.Name | printf "%-11s"}} {{.Short}}{{end}}{{end}}
+
+Use "gmc help [command]" for more information about a command.
+
+`
+
+var helpTemplate = `{{if .Runnable}}usage: gmc {{.UsageLine}}
+
+{{end}}{{.Long}}
+`
+
+// tmpl executes the given template text on data, writing the result to w.
+func tmpl(w io.Writer, text string, data interface{}) {
+	t := template.New("top")
+	template.Must(t.Parse(text))
+	if err := t.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
+
+func printUsage(w io.Writer) {
+	tmpl(w, usageTemplate, commands)
+}
+
+func usage() {
+	printUsage(os.Stderr)
+	os.Exit(2)
+}
+
+// help implements the 'help' command.
+func help(args []string) {
+	if len(args) == 0 {
+		printUsage(os.Stdout)
+		return
+	}
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "usage: gmc help command\n\nToo many arguments given.\n")
+		os.Exit(2) // failed at 'gmc help'
+	}
+
+	arg := args[0]
+
+	for _, cmd := range commands {
+		if cmd.Name() == arg {
+			tmpl(os.Stdout, helpTemplate, cmd)
+			return
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "Unknown help topic %#q. Run 'gmc help'.\n", arg)
+	os.Exit(2) // failed at 'go help cmd'
+}
